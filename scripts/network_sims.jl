e14a7b7ca4fe4c39291d12107db2528b31c8af2b
John Palmer <johnrbpalmer@gmail.com>
2021-08-21 20:40:52 +0200

    initial sim working

--- scripts/network_sims.jl
+++ scripts/network_sims.jl
@@ -1,18 +1,19 @@
-using Base: Multimedia
 using Random, Distributions
+using ProgressMeter, Plots
+gr()
 
 Random.seed!(123)
+
+n_steps = 100
+n_humans = 5000
+mean_bites_per_person = 10
+n_mosquitoes = 10000
+mean_bites_per_mosquito = 2
 
 latent_period = 3
 recovery_period = 3
 
-transmission_prob = 1
-
-n_humans = 50
-mean_bites_per_person = 10
-
-n_mosquitoes = 100
-mean_bites_per_mosquito = 2
+transmission_prob = .001
 
 human_distribution = Beta(.5, .5)
 mosquito_distribution = Beta(.5, .5)
@@ -26,18 +27,36 @@ status_humans[rand(1:n_humans, 1)[1]] = 1
 
 status_mosquitoes = zeros(Int8, n_mosquitoes)
 
+n_human_infections = Vector{Int64}(undef, n_steps)
+n_mosquito_infections = Vector{Int64}(undef, n_steps)
+
+n_human_infections[1] = 1
+n_mosquito_infections[1] = 0
+
+@showprogress for s = 2:n_steps
+
 # human-to-mosquito infections
-for h in findall(status_humans .== 1), m in findall(status_mosquitoes .==0)
-  if rand(Bernoulli(human_probs[h]*mosquito_probs[m]*transmission_prob), 1)[1]
-    status_mosquitoes[m] = 1
+  for h in findall(status_humans .== 1), m in findall(status_mosquitoes .==0)
+    if rand(Bernoulli(human_probs[h]*mosquito_probs[m]*transmission_prob), 1)[1]
+      status_mosquitoes[m] = 1
+    end
   end
+
+n_mosquito_infections[s] = sum(status_mosquitoes)
+
+  # mosquito-human infections
+  for h in findall(status_humans .== 0), m in findall(status_mosquitoes .==1)
+    if rand(Bernoulli(human_probs[h]*mosquito_probs[m]*transmission_prob), 1)[1]
+      status_humans[h] = 1
+    end
+  end
+
+  n_human_infections[s] = sum(status_humans)
+
 end
 
-# mosquito-human infections
-for h in findall(status_humans .== 0), m in findall(status_mosquitoes .==1)
-  if rand(Bernoulli(human_probs[h]*mosquito_probs[m]*transmission_prob), 1)[1]
-    status_humans[h] = 1
-  end
-end
+using Plots
+gr()
 
+plot(1:n_steps, n_human_infections)
 
