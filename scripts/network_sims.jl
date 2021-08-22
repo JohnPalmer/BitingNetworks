b1a09f48a798a3e25cf4f123d0fdc9bf37b9ffe1
John Palmer <johnrbpalmer@gmail.com>
2021-08-22 20:22:39 +0200

    moved main work into functions in src directory but now about to remove it as I have moved them into their own Bites.jl package

--- scripts/network_sims.jl
+++ scripts/network_sims.jl
@@ -2,14 +2,18 @@ using Random, Distributions
 using ProgressMeter, Plots, StatsPlots
 gr()
 
+include("src/biting_network_functions.jl")
+
 Threads.nthreads()
 
 Random.seed!(123)
 
-const n_steps = 50
-const n_reps = 300
-const n_humans = 1000
-const n_mosquitoes = 10000
+n_steps = 50
+n_reps = 60
+n_humans = 1000
+n_mosquitoes = 4000
+
+transmission_prob = .06
 
 # how long before infected human recovers
 const human_infection_time = 3
@@ -26,8 +30,6 @@ const neg_bin_p_a = .21
 const neg_bin_r_b = 10
 const neg_bin_p_b = .35
 
-const transmission_prob = .1
-
 human_distribution = NegativeBinomial(neg_bin_r, neg_bin_p)
 
 human_distribution_a = NegativeBinomial(neg_bin_r_a, neg_bin_p_a)
@@ -36,7 +38,7 @@ human_distribution_b = NegativeBinomial(neg_bin_r_b, neg_bin_p_b)
 
 # human_distribution = Beta(2, 5)
 # mosquito_distribution = Beta(.2, 2)
-mosquito_probs1 = fill(1/n_humans, (n_mosquitoes,1))
+mosquito_probs1 = fill(1/n_humans, n_mosquitoes)
 
 human_probs1 = rand(human_distribution, n_humans)/(sum(mosquito_probs1))
 
@@ -44,8 +46,8 @@ human_probs1a = rand(human_distribution_a, n_humans)/(sum(mosquito_probs1))
 
 human_probs1b = rand(human_distribution_b, n_humans)/(sum(mosquito_probs1))
 
-human_probs2 = fill(mean(human_probs1), (n_humans,1))
-mosquito_probs2 = fill(mean(mosquito_probs1), (n_mosquitoes,1))
+human_probs2 = fill(mean(human_probs1), n_humans)
+mosquito_probs2 = fill(mean(mosquito_probs1), n_mosquitoes)
 
 
 human_distribution3 = Uniform(0, mean(human_probs1)*2)
@@ -59,7 +61,7 @@ scenario_results = []
 
 scenarios = [[mosquito_probs1, human_probs1],[mosquito_probs1, human_probs1a], [mosquito_probs1, human_probs1b],[mosquito_probs2, human_probs2], [mosquito_probs3, human_probs3] ]
 
-scenario = scenarios[3]
+scenario = scenarios[4]
 
 for scenario in scenarios
 
@@ -97,83 +99,15 @@ for scenario in scenarios
 
   n_human_recovered_reps = Array{Int64}(undef, n_reps, n_steps)
 
+  r = 1
+
   p = Progress(n_reps)
 
   Threads.@threads for r = 1:n_reps
 
-  # vectors of infections statusas follows: 0 = susceptible, >0 = infected, <0 = recovered. Everyone starts susceptible
-  status_humans = zeros(Int8, n_humans)
-  status_mosquitoes = zeros(Int8, n_mosquitoes)
 
-  # vector of mosquito ages
-  age_mosquitoes = rand(DiscreteUniform(0, mosquito_life_span), n_mosquitoes)
+    n_mosquito_infections_reps[r, :], n_human_infections_reps[r, :], n_human_recovered_reps[r, :] = bite_steps(n_steps, n_humans, n_mosquitoes, human_infection_time, mosquito_life_span, human_probs, mosquito_probs, transmission_prob)
 
-  # creating first infection
-  status_humans[rand(1:n_humans, 1)[1]] = 1
-
-  n_human_infections = Vector{Int64}(undef, n_steps)
-  n_mosquito_infections = Vector{Int64}(undef, n_steps)
-
-  n_human_recovered = Vector{Int64}(undef, n_steps)
-
-  n_human_infections[1] = 1
-  n_mosquito_infections[1] = 0
-  n_human_recovered[1] = 0
-
-    for s = 2:n_steps
-
-      # update statuses based on time n_steps
-      status_humans[findall(status_humans .> 0)] .+= 1
-      status_humans[findall(status_humans .> human_infection_time)] .= -1
-
-      # all mosquitoes age by 1 step
-      age_mosquitoes .+= 1
-      # mosquitoes over max age die and are reborn (under assumption of stable population) as susceptible
-      status_mosquitoes[findall(age_mosquitoes .> mosquito_life_span)] .= 0
-      age_mosquitoes[findall(age_mosquitoes .> mosquito_life_span)] .= 0
-
-      # find indexes of infected humans (status>0)
-      i_hs = findall(status_humans .> 0)
-      # find indexes of susecptible mosquitoes (status == 0)
-      s_ms = findall(status_mosquitoes .==0)
-      
-      # calculate number of iterations needed to cycle through all combinations. (This is useful if parallizing over these iterations, since it allows for more efficient distribution of the tasks)
-      these_iterations = 1:length(i_hs)*length(s_ms)
-
-    # human-to-mosquito infections
-      for i in these_iterations
-        h = i_hs[cld(i, length(s_ms))]
-        m = s_ms[mod1(i, length(s_ms))]
-        if rand(Bernoulli(human_probs[h]*mosquito_probs[m]*transmission_prob), 1)[1]
-          status_mosquitoes[m] = 1
-        end
-      end
-
-    n_mosquito_infections[s] = sum(status_mosquitoes .> 0)
-
-    s_hs = findall(status_humans .== 0)
-    i_ms = findall(status_mosquitoes .==1)
-    these_iterations = 1:length(s_hs)*length(i_ms)
-
-      # mosquito-human infections
-      for i in these_iterations
-        h = s_hs[cld(i, length(i_ms))]
-        m = i_ms[mod1(i, length(i_ms))]
-        if rand(Bernoulli(human_probs[h]*mosquito_probs[m]*transmission_prob), 1)[1]
-          status_humans[h] = 1
-        end
-      end
-
-      n_human_infections[s] = sum(status_humans.>0)
-      n_human_recovered[s] = sum(status_humans.<0)
-
-    end
-
-    n_human_infections_reps[r, :] = n_human_infections
-
-    n_mosquito_infections_reps[r, :] = n_mosquito_infections
-
-    n_human_recovered_reps[r, :] = n_human_recovered
 
     next!(p)
 
@@ -216,8 +150,6 @@ end
 
 R0 = [x[2] for x in scenario_results]
 
-scenario_results[1][6][:,n_steps] .+ scenario_results[1][1][:,n_steps] 
-
 AR = [ [mean(x[6][:,n_steps] .+ x[1][:,n_steps]), quantile(x[6][:,n_steps] .+ x[1][:,n_steps], [.025, .25, .5, .75, .975])]  for x in scenario_results]
 
 mean_bites_per_person = [mean(x[4]) for x in scenario_results]
@@ -240,7 +172,7 @@ end
 
 max_bites = maximum([maximum(x[4]) for x in scenario_results])
 
-these_bite_plots = [density(x[4], xaxis = ("N Bites", (0, max_bites), 0:40:max_bites), yaxis = ("Density", (0, .1), 0:.02:.1), legend=:none) for x in scenario_results]
+these_bite_plots = [density(x[4], xaxis = ("N Bites", (0, max_bites), 0:40:max_bites), yaxis = ("Density", (0, .06)), legend=:none) for x in scenario_results]
 
 l = @layout [a f; b g; c h; d i; e j]
 
