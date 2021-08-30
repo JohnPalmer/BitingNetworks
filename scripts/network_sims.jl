2169b4c9525dfd72782179d6e2d3bcbc95fd5bf4
John Palmer <johnrbpalmer@gmail.com>
2021-08-30 22:01:38 +0200

    about to move R0 calculations to package

--- scripts/network_sims.jl
+++ scripts/network_sims.jl
@@ -1,6 +1,6 @@
 using Random, Distributions
 using ProgressMeter, Plots, StatsPlots, KernelDensity, Bootstrap
-gr()
+plotlyjs()
 
 using Bites
 
@@ -23,39 +23,35 @@ const human_infection_time = 3
 # how long do mosquitoes live. Since mosquitoes do not usually get accute arbovirus infections, the life span is the key: They will go from susceptible->infected->dead/reborn. (With assumption of population stability, deaths simply put the agent back to susceptible status.) "Unlike arboviral infections in humans, which are usually acute, arboviral infections in mosquitoes are persistent. Once the infection is established, the mosquito remains infected for the rest of its life." https://www.sciencedirect.com/science/article/pii/S1931312819303701
 const mosquito_life_span = 20
 
-HD_const, MD_const = distribute_bite_probabilities( (1/n_mosquitoes):(1/n_mosquitoes), (1/n_humans):(1/n_humans), n_humans, n_mosquitoes, expected_bites)
+fixed_mosquito_probs = (1/n_humans):(1/n_humans)
 
+subpop_a_size = Int(n_humans/2)
+subpop_a = rand(Truncated(Normal(3, 3), 0, Inf), subpop_a_size)
 
-HD_uniform_0_1, MD_const = distribute_bite_probabilities(Distributions.Uniform(0,1), (1/n_humans):(1/n_humans), n_humans, n_mosquitoes, expected_bites)
+subpop_b_size = n_humans - subpop_a_size
+subpop_b = rand(Truncated(Normal(30, 3), 0, Inf), subpop_b_size)
 
-HD_levy_1_0001, MD_const = distribute_bite_probabilities(Distributions.Levy(1, .0001), (1/n_humans):(1/n_humans), n_humans, n_mosquitoes, expected_bites)
-
-HD_exp05, MD_const = distribute_bite_probabilities(Distributions.Exponential(1/.5), (1/n_humans):(1/n_humans), n_humans, n_mosquitoes, expected_bites)
-
-
-HD_truncnormal_1_3 = distribute_bite_probabilities(Truncated(Normal(1, 3), 0, Inf), (1/n_humans):(1/n_humans), n_humans, n_mosquitoes, expected_bites)[1]
-
-
-HD_trunclevy_1_3 = distribute_bite_probabilities(Truncated(Levy(1.1, 3), 1, 1000), (1/n_humans):(1/n_humans), n_humans, n_mosquitoes, expected_bites)[1]
-
+rand(subpop_b, 10)
 
 scenario_results = []
 
-scenarios = ( 
-  constant= [MD_const, HD_const],
-  uniform = [MD_const, HD_uniform_0_1],
-  trunclevy = [MD_const, HD_trunclevy_1_3],
-  exp = [MD_const, HD_exp05],
-  levy = [MD_const, HD_levy_1_0001])
+human_distributions = ( 
+  constant= (1/n_mosquitoes):(1/n_mosquitoes),
+  uniform = Uniform(0,1),
+  exp = Exponential(1/.5),
+  tlevy1p1 = Truncated(Levy(1.1, .0001), 1, 1000),
+  levy1p1 = Levy(1.1, .0001),
+  levy2p1 = Levy(2.1, .0001),
+  levy3p1 = Levy(3.1, .0001),
+  mixed_norms = vcat(subpop_a, subpop_b)
+)
 
 
-scenario = scenarios[4]
+  human_distribution = human_distributions[8]
 
-for scenario in scenarios
+for human_distribution in human_distributions
 
-  mosquito_probs = scenario[1]
-
-  human_probs = scenario[2]
+  human_probs, mosquito_probs = distribute_bite_probabilities(human_distribution, fixed_mosquito_probs, n_humans, n_mosquitoes, expected_bites)
 
   these_iterations = 1:length(human_probs)*length(mosquito_probs)
 
@@ -74,11 +70,7 @@ for scenario in scenarios
 
   human_bite_distribution = sum(bn, dims=2)
 
-#  density(human_bite_distribution)
-
-#  mean(human_bite_distribution)
-
-#  density(mosquito_bite_distribution)
+# density(human_bite_distribution)
 
   n_human_infections_reps = Array{Int64}(undef, n_reps, n_steps)
 
@@ -92,7 +84,8 @@ for scenario in scenarios
 
   Threads.@threads for r = 1:n_reps
 
-
+    human_probs, mosquito_probs = distribute_bite_probabilities(human_distribution, fixed_mosquito_probs, n_humans, n_mosquitoes, expected_bites)
+  
     n_mosquito_infections_reps[r, :], n_human_infections_reps[r, :], n_human_recovered_reps[r, :] = bite_steps(n_steps, n_humans, n_mosquitoes, human_infection_time, mosquito_life_span, human_probs, mosquito_probs, transmission_prob)
 
 
@@ -124,8 +117,12 @@ for i in 1:n_reps
   x = n_mosquito_infections_reps[i,:]
   if maximum(x) > 1
     this_min = minimum(x[x.>0])
-    this_next_min = minimum(x[x.>this_min])
-    R0s[i] = (this_next_min - this_min) / this_min
+    if sum(x.>this_min)>0
+      this_next_min = minimum(x[x.>this_min])
+      R0s[i] = (this_next_min - this_min) / this_min
+    else
+      R0s[i] = 0
+    end
   else
     R0s[i] = 0
   end
@@ -152,9 +149,9 @@ R0 = [x[2] for x in scenario_results]
 n_boot = 1000
 human_R0_boot = [stderror(bootstrap(mean, x[:human_R0_converg_check], BasicSampling(n_boot))) for x in scenario_results]
 
-these_labs = [string(x) for x in keys(scenarios)]
+these_labs = [string(x) for x in keys(human_distributions)]
 
-plot(transpose(human_R0_convergence_checks), label=reshape(these_labs, 1, length(scenario_results)), xlabel="Repititions", ylabel="Mean R0", legend=:bottomright, linewidth=2)
+plot(transpose(human_R0_convergence_checks), label=reshape(these_labs, 1, length(scenario_results)), xlabel="Repetitions", ylabel="Mean R0", legend=:bottomright, linewidth=2, palette = :Dark2_8)
 plot!(size=(800,600))
 png("plots/R0_convergence_check_plot.png")
 
@@ -163,7 +160,7 @@ AR = [ [mean(x[6][:,n_steps] .+ x[1][:,n_steps]), quantile(x[6][:,n_steps] .+ x[
 
 AR_convergence_check = [mean(x[6][1:i,n_steps] .+ x[1][1:i,n_steps]) for x in scenario_results, i in 1:n_reps]
 
-plot(transpose(AR_convergence_check), label=reshape([string("S", x) for x in 1:length(scenario_results)], 1, length(scenario_results)), xlabel="Repititions", ylabel="Mean Attack Rate")
+plot(transpose(AR_convergence_check), label=label=reshape(these_labs, 1, length(scenario_results)), xlabel="Repititions", ylabel="Mean Attack Rate", palette = :Dark2_8, linewidth=2)
 plot!(size=(800,600))
 png("plots/AR_convergence_check_plot.png")
 
@@ -176,17 +173,19 @@ max_bites_per_person = [maximum(x[4]) for x in scenario_results]
 
 maximum(scenario_results[1][1])
 
+using Plots.PlotMeasures
 these_plots = []
 
 this_max = maximum([maximum(x[1]) for x in scenario_results])
 
 steps_to_plot = 15
 
-for i in 1:length(scenarios)
+for i in 1:length(human_distributions)
   n_human_infections_reps = scenario_results[i][1]./n_humans
   n_human_infections_reps_median = transpose(median(n_human_infections_reps, dims=1))
 
-  this_p = plot(1:steps_to_plot, transpose(n_human_infections_reps[ :, 1:steps_to_plot]), w=.2, color=:grey, legend=:none, yaxis = ("Infected", (0, 1), 0:.5:1), xaxis = ("Time"))
+  this_p = plot(1:steps_to_plot, transpose(n_human_infections_reps[ :, 1:steps_to_plot]), w=.2, color=:grey, legend=:none, yaxis = ("Infected", (0, 1), 0:.5:1), xaxis = ("Time"), margin = 1cm)
+
 
   plot!(this_p, 1:steps_to_plot,n_human_infections_reps_median[1:steps_to_plot], w=2, color=:black, yaxis = ("Infected", (0, 1), 0:.2:1))
 
@@ -197,10 +196,27 @@ max_bites = maximum([maximum(x[4]) for x in scenario_results])
 
 kd_max = maximum(kde(vec(scenario_results[1][4])).density)
 
-these_bite_plots = [density(scenario_results[i][:human_bite_distribution], xaxis = ("N Bites", (0, max_bites), 0:50:max_bites), yaxis = ("Density", (-0.01, kd_max)), legend=:none, normed=true, linewidth=2, title=these_labs[i]) for i in 1:length(scenario_results)]
+these_bite_plots = [density(scenario_results[i][:human_bite_distribution], xaxis = ("N Bites", (0, max_bites), 0:ceil(max_bites/10):max_bites), yaxis = ("Density", (-0.01, kd_max)), legend=:none, normed=true, linewidth=2, title=these_labs[i], margin = 1cm) for i in 1:length(scenario_results)]
 
-l = @layout [a f; b g; c h; d i; e j]
 
-plot(these_plots[1], these_bite_plots[1], these_plots[2], these_bite_plots[2], these_plots[3], these_bite_plots[3],these_plots[4], these_bite_plots[4], these_plots[5], these_bite_plots[5], layout = l)
-plot!(size=(800,800))
-png("plots/epicurve_comparison.png")
+this_p = plot( 
+  these_bite_plots[1], 
+  these_plots[1], 
+  these_bite_plots[2], 
+  these_plots[2], 
+  these_bite_plots[3], 
+  these_plots[3], 
+  these_bite_plots[4], 
+  these_plots[4], 
+  these_bite_plots[5], 
+  these_plots[5], 
+  these_bite_plots[6], 
+  these_plots[6], 
+  these_bite_plots[7], 
+  these_plots[7], 
+  these_bite_plots[8], 
+  these_plots[8], 
+  layout=(8,2)
+)
+plot!(this_p, size=(800,2000))
+savefig(this_p, "plots/epicurve_comparison.html")
