50f15f9bacdabfae5aa45148eb3a4cb1114b9d6c
John Palmer <johnrbpalmer@gmail.com>
2021-08-31 02:11:21 +0200

    about to start new sims

--- scripts/network_sims.jl
+++ scripts/network_sims.jl
@@ -1,8 +1,8 @@
-using Random, Distributions
+using DrWatson
+quickactivate(@__DIR__, "BitingNetworks")
+using Bites, Random, Distributions
 using ProgressMeter, Plots, StatsPlots, KernelDensity, Bootstrap
 plotlyjs()
-
-using Bites
 
 Threads.nthreads()
 
@@ -23,20 +23,22 @@ const human_infection_time = 3
 # how long do mosquitoes live. Since mosquitoes do not usually get accute arbovirus infections, the life span is the key: They will go from susceptible->infected->dead/reborn. (With assumption of population stability, deaths simply put the agent back to susceptible status.) "Unlike arboviral infections in humans, which are usually acute, arboviral infections in mosquitoes are persistent. Once the infection is established, the mosquito remains infected for the rest of its life." https://www.sciencedirect.com/science/article/pii/S1931312819303701
 const mosquito_life_span = 20
 
-fixed_mosquito_probs = (1/n_humans):(1/n_humans)
+fixed_mosquito_prob = (1/n_humans)
+mosquito_distribution = fixed_mosquito_prob:fixed_mosquito_prob
+mosquito_distribution_name = string("fixed_", fixed_mosquito_prob)
 
 subpop_a_size = Int(n_humans/2)
-subpop_a = rand(Truncated(Normal(3, 3), 0, Inf), subpop_a_size)
+subpop_a_distribution = Truncated(Normal(3, 3), 0, Inf)
+subpop_a = rand(subpop_a_distribution, subpop_a_size)
 
 subpop_b_size = n_humans - subpop_a_size
-subpop_b = rand(Truncated(Normal(30, 3), 0, Inf), subpop_b_size)
-
-rand(subpop_b, 10)
+subpop_b_distribution = Truncated(Normal(30, 3), 0, Inf)
+subpop_b = rand(subpop_b_distribution, subpop_b_size)
 
 scenario_results = []
 
 human_distributions = ( 
-  constant= (1/n_mosquitoes):(1/n_mosquitoes),
+  constant = (1/n_mosquitoes):(1/n_mosquitoes),
   uniform = Uniform(0,1),
   exp = Exponential(1/.5),
   tlevy1p1 = Truncated(Levy(1.1, .0001), 1, 1000),
@@ -46,12 +48,19 @@ human_distributions = (
   mixed_norms = vcat(subpop_a, subpop_b)
 )
 
+human_distribution_names = join([string(x) for x in keys(human_distributions)], ".")
 
-  human_distribution = human_distributions[8]
+# savename(this_sim_dict)
 
-for human_distribution in human_distributions
 
-  human_probs, mosquito_probs = distribute_bite_probabilities(human_distribution, fixed_mosquito_probs, n_humans, n_mosquitoes, expected_bites)
+hdi = 1
+
+for hdi in 1:length(human_distributions)
+
+  human_distribution = human_distributions[hdi]
+  human_distribution_name = string(keys(human_distributions)[hdi])
+
+  human_probs, mosquito_probs = distribute_bite_probabilities(human_distribution, mosquito_distribution, n_humans, n_mosquitoes, expected_bites)
 
   these_iterations = 1:length(human_probs)*length(mosquito_probs)
 
@@ -84,81 +93,65 @@ for human_distribution in human_distributions
 
   Threads.@threads for r = 1:n_reps
 
-    human_probs, mosquito_probs = distribute_bite_probabilities(human_distribution, fixed_mosquito_probs, n_humans, n_mosquitoes, expected_bites)
+    human_probs, mosquito_probs = distribute_bite_probabilities(human_distribution, mosquito_distribution, n_humans, n_mosquitoes, expected_bites)
   
     n_mosquito_infections_reps[r, :], n_human_infections_reps[r, :], n_human_recovered_reps[r, :] = bite_steps(n_steps, n_humans, n_mosquitoes, human_infection_time, mosquito_life_span, human_probs, mosquito_probs, transmission_prob)
-
 
     next!(p)
 
   end
 
-  R0s = Vector(undef, n_reps)
+  human_R0_results = calculate_r0(n_reps, n_human_infections_reps, 1)
+  mosquito_R0_results = calculate_r0(n_reps, n_mosquito_infections_reps, 0)
 
-  for i in 1:n_reps
-    x = n_human_infections_reps[i,:]
-    if maximum(x) > 1
-      R0s[i] = minimum(x[x.>1])
-    else
-      R0s[i] = 0
-    end
-  end
+scenario_result = (
+  n_human_infections_reps = n_human_infections_reps, 
+  human_R0=human_R0_results.R0, 
+  mosquito_R0 = mosquito_R0_results.R0, 
+  human_bite_distribution = human_bite_distribution, 
+  mosquito_bite_distribution = mosquito_bite_distribution,
+  n_human_recovered_reps = n_human_recovered_reps,
+  human_R0_converge_check = human_R0_results.converge_check,
+  mosquito_R0_converge_check = mosquito_R0_results.converge_check,
+  human_R0_reps = human_R0_results.R0_reps,
+  mosquito_R0_reps= mosquito_R0_results.R0_reps, name = human_distribution_name,
+  n_steps = 50,
+  n_reps = 300,
+  n_humans = 1000,
+  n_mosquitoes = 4000,
+  trans_prob = .4, 
+  exp_bites = n_mosquitoes*2.0,
+  human_infect_time = 3,
+  mosq_life_span = 20,
+  human_dist = human_distribution,
+  human_dist_name = human_distribution_name,
+  mosquito_dist = mosquito_distribution,
+  mosquito_dist_name = mosquito_distribution_name
+  )
 
-human_R0 = mean(R0s)
-
-human_R0_converg_check = Vector(undef, n_reps)
-for i in 1:n_reps
-  human_R0_converg_check[i] = mean(R0s[1:i])
-end
-
-R0s = Vector(undef, n_reps)
-
-for i in 1:n_reps
-  x = n_mosquito_infections_reps[i,:]
-  if maximum(x) > 1
-    this_min = minimum(x[x.>0])
-    if sum(x.>this_min)>0
-      this_next_min = minimum(x[x.>this_min])
-      R0s[i] = (this_next_min - this_min) / this_min
-    else
-      R0s[i] = 0
-    end
-  else
-    R0s[i] = 0
-  end
-end
-
-mosquito_R0 = mean(R0s)
-
-mosquito_R0_converg_check = Vector(undef, n_reps)
-for i in 1:n_reps
-  mosquito_R0_converg_check[i] = mean(R0s[1:i])
-end
-
-
-scenario_result = (n_human_infections_reps=n_human_infections_reps, human_R0=human_R0, mosquito_R0=mosquito_R0, human_bite_distribution=human_bite_distribution, mosquito_bite_distribution=mosquito_bite_distribution, n_human_recovered_reps=n_human_recovered_reps, human_R0_converg_check=human_R0_converg_check, mosquito_R0_converg_check=mosquito_R0_converg_check)
+@tagsave(datadir("sims", savename(scenario_result, "jld2")), tostringdict(ntuple2dict(scenario_result)))
 
 push!(scenario_results, scenario_result)
 
 end
 
-human_R0_convergence_checks = [mean(x[:human_R0_converg_check][1:i]) for x in scenario_results, i in 1:n_reps]
 
-R0 = [x[2] for x in scenario_results]
+####
+human_R0_convergence_checks = [x[:human_R0_converge_check] for x in scenario_results]
 
-n_boot = 1000
-human_R0_boot = [stderror(bootstrap(mean, x[:human_R0_converg_check], BasicSampling(n_boot))) for x in scenario_results]
+
+R0 = [x.human_R0 for x in scenario_results]
 
 these_labs = [string(x) for x in keys(human_distributions)]
 
-plot(transpose(human_R0_convergence_checks), label=reshape(these_labs, 1, length(scenario_results)), xlabel="Repetitions", ylabel="Mean R0", legend=:bottomright, linewidth=2, palette = :Dark2_8)
+plot(human_R0_convergence_checks, label=reshape(these_labs, 1, length(scenario_results)), xlabel="Repetitions", ylabel="Mean R0", legend=:bottomright, linewidth=2, palette = :Dark2_8)
 plot!(size=(800,600))
 png("plots/R0_convergence_check_plot.png")
 
 
-AR = [ [mean(x[6][:,n_steps] .+ x[1][:,n_steps]), quantile(x[6][:,n_steps] .+ x[1][:,n_steps], [.025, .25, .5, .75, .975])] for x in scenario_results]
+AR = [ [mean(x[:n_human_recovered_reps][:,n_steps] .+ x[:n_human_infections_reps][:,n_steps]), quantile(x[6][:,n_steps] .+ x[1][:,n_steps], [.025, .25, .5, .75, .975])] for x in scenario_results]
 
-AR_convergence_check = [mean(x[6][1:i,n_steps] .+ x[1][1:i,n_steps]) for x in scenario_results, i in 1:n_reps]
+AR_convergence_check = [mean(x[n_human_recovered_reps][:n_human_infections_reps:i,n_steps] .+ x[1][1:i,n_steps]) for x in scenario_results, i in 1:n_reps]
 
 plot(transpose(AR_convergence_check), label=label=reshape(these_labs, 1, length(scenario_results)), xlabel="Repititions", ylabel="Mean Attack Rate", palette = :Dark2_8, linewidth=2)
 plot!(size=(800,600))
