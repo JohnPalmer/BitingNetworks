30e212053b356c6032cdf11dd3b4f186f89ed19f
John Palmer <johnrbpalmer@gmail.com>
2022-06-05 12:27:06 +0200

    uncommited changes from last summer

---
+++ notes/lit_review.md
@@ -0,0 +1,10 @@
+# Literature Review Notes
+
+## Network theory
+
+### Hypergraphs
+
+* Influential groups for seeding and sustaining nonlinear contagion in heterogeneous hypergraphs: https://www.nature.com/articles/s42005-021-00788-w
+
+* https://arxiv.org/abs/2201.07056
+
--- scripts/r_visualization.R
+++ scripts/r_visualization.R
@@ -34,3 +34,37 @@ these_medians = plot_data %>% group_by(measure_f) %>% summarise(med = median(val
 
 ggplot(plot_data, aes(x=value, y=variable, fill=measure_f)) + stat_halfeye(normalize="groups", alpha=.7, point_interval = mean_qi,.width = c(0.66, 0.95)) + facet_wrap(~measure_f, scale="free_x", nrow = 1) + theme(legend.position="none") + scale_fill_brewer(palette = "Set1") + xlab("") + ylab("")# + geom_vline(data = these_medians, aes(xintercept=med, color=measure_f)) 
 ggsave("plots/bcn_districts_combo_vri_hbd_r0_AR.png", width=8, height=4)
+
+
+# theoretical distributions ####
+
+combo_data = "data/sim_summaries/combo_plot_AR_R0_eb=800.0_hit=3_mls=20_n_h=100_n_m=400_n_r=2000_n_s=1000_this_set_name=cuel12_tp=0.1.csv"
+
+hd_data = "data/sim_summaries/human_bite_distributions_eb=800.0_hit=3_mls=20_n_h=100_n_m=400_n_r=2000_n_s=1000_this_set_name=cuel12_tp=0.1.csv"
+
+plot_data <- read_csv(combo_data)
+
+ggplot(plot_data, aes(x=value, y=variable)) + stat_halfeye() + facet_wrap(~measure, scale="free_x")
+
+
+hbd = read_csv(hd_data) %>% pivot_longer(cols=everything(), names_to="variable", values_to="value") %>% mutate(measure = "Bites per person")
+
+ggplot(hbd, aes(x=value, y=variable)) + stat_halfeye() #+ facet_wrap(~measure, scale="free_x"))
+
+ggplot(hbd, aes(x=value, color=variable)) + geom_density() #+ facet_wrap(~measure, scale="free_x"))
+
+
+unique(plot_data$variable)
+
+plot_data = bind_rows(hbd, plot_data) %>% mutate(measure_f = factor(measure, levels=c("Bites per person", "R0", "AR")), variable_f = factor(variable, levels=c("exp", "uniform", "levy1p1", "levy2p1", "constant"), labels = c("exponenaial", "uniform", "levy 1.1", "levy 2.1", "constant")))
+
+these_medians = plot_data %>% group_by(measure_f) %>% summarise(med = median(value))
+
+this_pal = brewer.pal(4, "Set1")[2:4]
+
+ggplot(plot_data, aes(x=value, y=variable_f, fill=measure_f)) + stat_halfeye(normalize="groups", alpha=.7, point_interval = mean_qi,.width = c(0.66, 0.95)) + facet_wrap(~measure_f, scale="free_x", nrow = 1) + theme(legend.position="none") + scale_fill_manual(values=this_pal) + xlab("") + ylab("")# + geom_vline(data = these_medians, aes(xintercept=med, color=measure_f)) 
+ggsave("plots/theoretical_dist_combo_vri_hbd_r0_AR.png", width=8, height=4)
+
+
+
+
