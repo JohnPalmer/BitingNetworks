1a85e6e8118942cee8574262701e7168e3807ac4
John Palmer <johnrbpalmer@gmail.com>
2022-06-06 17:00:32 +0200

    back to 10k reps

--- scripts/network_sims.jl
+++ scripts/network_sims.jl
@@ -9,7 +9,7 @@ Threads.nthreads()
 Random.seed!(123)
 
 n_s = 1000 # number of time steps (e.g. days)
-n_r = 30000 # number of repetitions to simulate
+n_r = 10000 # number of repetitions to simulate
 n_h = 100 # number of humans in population
 n_m = 800 # number of mosquitoes in population
 
